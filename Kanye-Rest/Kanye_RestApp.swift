ed29a069ef4f7046b89a825151cc57e05ad60f4c
MikhailLukyanov1 <lukyanov.m@etoncollege.org.uk>
2023-03-17 18:59:26 +0000

    Initial Commit

---
+++ Kanye-Rest/Kanye_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kanye_RestApp.swift
+//  Kanye-Rest
+//
+//  Created by MIKHAEL LUKYANOV on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Kanye_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
